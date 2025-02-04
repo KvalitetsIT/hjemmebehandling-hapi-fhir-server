c3fb42f94ef42de0ccd1389b2da5888ef261e252
Jonas <jqh@kvalitetsit.dk>
2025-02-04 15:58:54 +0100

    corrected integrationtest/pom

--- build/build.sh
+++ build/build.sh
@@ -5,4 +5,4 @@ docker build -t resources -f ./integrationtest/docker/Dockerfile-resources --no-
 docker run -d --name hjemmebehandling-hapi-fhir-server-resources resources
 
 # Build inside docker container
-docker run -v /var/run/docker.sock:/var/run/docker.sock  -v $HOME/.docker/config.json:/root/.docker/config.json -v $(pwd):/src -v $HOME/.m2:/root/.m2 --volumes-from hjemmebehandling-hapi-fhir-server-resources maven:3-ibm-semeru-21-jammy  /src/build/maven.sh
+docker run -v /var/run/docker.sock:/var/run/docker.sock  -v $HOME/.docker/config.json:/root/.docker/config.json -v $(pwd):/src -v $HOME/.m2:/root/.m2 -v /tmp:/tmp maven:3-ibm-semeru-21-jammy /src/build/maven.sh
